1dcd0eeabbcfef23df6370b744e5c5339143727d
OIIUOI <eunjin4788@gmail.com>
2022-09-16 23:59:46 +0900

    220916 problem solve

---
+++ 문제풀이/backjoon/220916/1431.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def sum_num(x):
+    result = 0
+    for i in x:
+        if i in number:
+            result += int(i)
+    return result
+
+number = ['0','1','2','3','4','5','6','7','8','9']
+guitar = []
+
+for _ in range(int(input())):
+    guitar.append(input().rstrip())
+
+guitar.sort(key= len)
+guitar.sort(key=sum_num, reverse=True)
+print(guitar)
